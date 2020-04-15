fa8f43b5db785462dd1d785920fc43bb5bbb6c49
Emmanuel Rocha <emmanuelrocha001@gmail.com>
2020-04-14 20:58:57 -0500

    user login endpoint added

--- api/models/user.js
+++ api/models/user.js
@@ -10,8 +10,10 @@ const userSchema = mongoose.Schema({
         unique: true,
         match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     },
-    username: {type: String, required: true },
+    firstName: {type: String, required: true },
+    lastName: { type: String, required: true },
     password: { type: String, required: true }
+
 });
 
 //constructor based on schema
--- api/routes/user.js
+++ api/routes/user.js
@@ -16,59 +16,81 @@ router.post('/signup', (req, res, next) => {
         });
       }
       else {
-          User.find( {username: req.body.username } )
-            .exec()
-            .then( user => {
 
-              if( user.length >= 1 ) {
-                return res.status(409).json({
-                  successful: false,
-                  message: 'username is already linked to an existing account'
-                });
-              } else {
-
-                bcrypt.hash(req.body.password, 10, (err, hash) =>{
-                  if (err) {
-                    return res.status(500).json({
-                        successful: false,
-                        error: err
-                    });
-                  } else {
-
-                    console.log(hash);
-
-                    const user = new User({
-                      _id: new mongoose.Types.ObjectId(),
-                      email: req.body.email,
-                      username: req.body.username,
-                      password: hash
-                    });
-
-                    //status 201 when creating resource
-                    user
-                      .save()
-                      .then( result => {
-                        res.status(201).json({
-                          successful: true,
-                          message: 'user account created successfully',
-                        });
-
-                      })
-                      .catch(err => {
-                        return res.status(500).json({
-                          successful: false,
-                          error: err
-                        });
-
-                      });
-                  }
-                });
-              }
+        bcrypt.hash(req.body.password, 10, (err, hash) =>{
+          if (err) {
+            return res.status(500).json({
+                successful: false,
+                error: err
             });
+          } else {
+
+            console.log(hash);
+
+            const user = new User({
+              _id: new mongoose.Types.ObjectId(),
+              email: req.body.email,
+              firstName: req.body.firstName,
+              lastName: req.body.lastName,
+              password: hash
+            });
+
+            //status 201 when creating resource
+            user
+              .save()
+              .then( result => {
+                res.status(201).json({
+                  successful: true,
+                  userId: result._id,
+                  message: 'user account created successfully',
+                });
+
+              })
+              .catch(err => {
+                return res.status(500).json({
+                  successful: false,
+                  error: err
+                });
+
+              });
+          }
+        });
+              
       }
     });
 });
 
+
+router.post('/login', (req, res, next) => {
+
+  User.find( {email: req.body.email} )
+    .exec()
+    .then(user => {
+      if (user.length < 1) {
+        return res.status(409).json({
+          successful: false,
+          message: 'Authentication failed, the email provided is not linked to an existing account.'
+        });
+      }
+      else {
+        bcrypt.compare( req.body.password, user[0].password, ( err, result) => {
+          if ( result ) {
+            return res.status(200).json({
+              successful: true,
+              message: 'Authentication successful',
+              userId: user[0]._id
+            });
+
+          } else {
+            return res.status(401).json({
+              successful: false,
+              message: 'Authentication failed, incorrect password.'
+            });
+          }
+        });
+      }
+    });
+});
 
 //export such that module can be used in other files
 module.exports = router;
