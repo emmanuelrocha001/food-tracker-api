585f7c17dc0bcc26c6754956fe18164f578a1346
Emmanuel Rocha <emmanuelrocha001@gmail.com>
2020-04-16 23:50:45 -0500

    changes

--- api/routes/user.js
+++ api/routes/user.js
@@ -10,7 +10,7 @@ const path = require('path');
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     // execute cb, pass potential error and path
-    cb(null, './uploads/');
+    cb(null, path.join(__dirname + '../../../uploads/'));
 
   },
   filename: (req, file, cb) => {
